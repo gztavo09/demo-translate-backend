b4a10bc1acf36e19c48bee860f890484cba646ad
Gustavo Arias <gjarias@arqsoft.com.pe>
2023-01-14 16:48:24 -0500

    new io

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ io.on('connection', socket => {
 });
 
 // Escuchar en un puerto específico
-const port = 3000;
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
     console.log(`Servidor escuchando en el puerto ${port}`);
 });
